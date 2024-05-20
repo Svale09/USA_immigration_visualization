8ba1a579e1ab569ec605f05fd0a392af06728c46
Svale09 <svalinaivan09+git@gmail.com>
2024-05-20 23:20:47 +0200

    Deleted unwanted rows from airports_dataset, dropped duplicates and saved it as a new CSV file

---
+++ data/airports_data_manipulation.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('/Users/ivansvalina/Documents/Faks/Vizualizacija podataka/KV projekt/project/.venv/lib/python3.12/site-packages')
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv(
+    '/Users/ivansvalina/Documents/Faks/Vizualizacija podataka/KV projekt/project/data/airports_dataset.csv',
+    delimiter=';')
+
+df.info()
+
+df = df.drop(df.columns[[3,4,5,6,7,8,9]], axis = 1)
+
+df.drop_duplicates(inplace=True)
+
+df.info()
+
+print(df.head(5))
+
+
