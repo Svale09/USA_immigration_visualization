137ff5ed476cdcb3ca2c8de06585c68cabff8691
Svale09 <svalinaivan09+git@gmail.com>
2024-05-21 22:50:22 +0200

    Finished the processing and preparation of individual datasets

--- data/land_border_data_manipulation.py
+++ data/land_border_data_manipulation.py
@@ -1,28 +1,34 @@
 import sys
-sys.path.append('/Users/ivansvalina/Documents/Faks/Vizualizacija podataka/KV projekt/project/.venv/lib/python3.12/site-packages')
-
 import pandas as pd
 import numpy as np
 import calendar
 
+sys.path.append('/Users/ivansvalina/Documents/Faks/Vizualizacija podataka/KV projekt/project/.venv/lib/python3.12/site-packages')
+
 df = pd.read_csv('data/land-border-data.csv')
 
-df = df.drop(df.columns[[2,7,8]], axis=1)
+df = df.drop(columns=[df.columns[2], df.columns[7], df.columns[8]])
 
-passengersDF = df[df['Measure'].str.contains('passengers|pedestrians', case=False, na=False)]
+passengersDF = df[df['Measure'].str.contains('passengers|pedestrians', case=False, na=False)].copy()
 
-passengersDF['Year'] = passengersDF['Date'].apply(lambda x: x.split()[1])
-passengersDF['Month'] = passengersDF['Date'].apply(lambda x: x.split()[0])
-
+passengersDF[['Month', 'Year']] = passengersDF['Date'].str.split(expand=True)
 passengersDF['Month'] = passengersDF['Month'].apply(lambda x: list(calendar.month_abbr).index(x))
 
 aggregated_data = passengersDF.groupby(['Port Name', 'Year', 'Month']).agg(
-    total_crossings = ('Value', 'sum'),
-    State = ('State', 'first'),
-    Point = ('Point', 'first'),
+    total_crossings=('Value', 'sum'),
+    State=('State', 'first'),
+    Point=('Point', 'first'),
 ).reset_index()
 
 aggregated_data['Month'] = aggregated_data['Month'].apply(lambda x: calendar.month_name[x])
 
-aggregated_data.info()
+aggregated_data['Point'] = aggregated_data['Point'].str.extract(r'\((.*?)\)')
+
+aggregated_data.to_csv('border_crossing_final.csv', index=False)
+aggregated_data.to_json('border_crossing_finalJSON.json', orient='records')
+
+print(aggregated_data.info())
 print(aggregated_data.head(30))
+
+
+
