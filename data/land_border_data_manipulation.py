d5a80d1273288004ff3dba562bfff3a65ae2203a
Svale09 <svalinaivan09+git@gmail.com>
2024-05-21 20:59:59 +0200

    Started processing the land border crossings data

---
+++ data/land_border_data_manipulation.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('/Users/ivansvalina/Documents/Faks/Vizualizacija podataka/KV projekt/project/.venv/lib/python3.12/site-packages')
+
+import pandas as pd
+import numpy as np
+
