40dd57a05ac4a29b83c196b05cf404f3046f897a
Svale09 <svalinaivan09+git@gmail.com>
2024-05-21 20:20:19 +0200

    Finished the preparation of the flights data. Removed unnecessary columns and aggregated the data by airport and month so the dataset represents the traffic of an airport during a month

--- data/flights_data_manipulation.py
+++ data/flights_data_manipulation.py
@@ -3,13 +3,28 @@ sys.path.append('/Users/ivansvalina/Documents/Faks/Vizualizacija podataka/KV pro
 
 import pandas as pd
 import numpy as np
+import calendar
 
 df = pd.read_csv(
     '/Users/ivansvalina/Documents/Faks/Vizualizacija podataka/KV projekt/project/data/inbound-flight.csv'
 )
 
+df.info()
+
 df = df.drop(df.columns[[3,5,6,7,8,9,10,11,12,13,14]], axis = 1)
 
-sorted_df = df.sort_values(by='usg_apt')
+aggregated_data = df.groupby(['Year', 'Month', 'usg_apt']).agg(
+    flights_count = ('data_dte', 'size'),
+    total_passengers = ('Total', 'sum')
+).reset_index()
 
-print(sorted_df.head())
+aggregated_data['Month'] = aggregated_data['Month'].apply(lambda x: calendar.month_name[x])
+
+aggregated_data.to_csv('incoming_flights_final.csv', index=False)
+aggregated_data.to_json('incoming_flights_finalJSON.json', orient='records')
+
+
+
+print(aggregated_data.head(20))
+print(aggregated_data.sort_values(by='usg_apt').head(20))
+aggregated_data.info()
