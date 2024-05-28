a6eac6f0676802c673f5df0f26e282317d1bd476
Svale09 <svalinaivan09+git@gmail.com>
2024-05-28 22:13:36 +0200

    Added states column to the border_crossing coordinates dataset

--- data/add_portName_to_coordinatesdataset.py
+++ data/add_portName_to_coordinatesdataset.py
@@ -4,22 +4,23 @@ import pandas as pd
 border_crossing_df = pd.read_json("web/FINAL_border_crossing_JSON.json")
 coordinates_df = pd.read_json("web/coordinates_dataset.json")
 
-# Create a dictionary to map PortCode to PortName from the border_crossing_df
+# Create dictionaries to map PortCode to PortName and State from the border_crossing_df
 portname_map = border_crossing_df.groupby('PortCode')['Port Name'].first().to_dict()
+state_map = border_crossing_df.groupby('PortCode')['State'].first().to_dict()
 
-# Add PortName column to coordinates_df based on PortCode
+# Add PortName and State columns to coordinates_df based on PortCode
 coordinates_df['PortName'] = coordinates_df['PortCode'].map(portname_map)
+coordinates_df['State'] = coordinates_df['PortCode'].map(state_map)
 
 # Fill NaN values in case there are PortCodes in coordinates_df not present in border_crossing_df
 coordinates_df['PortName'].fillna('', inplace=True)
+coordinates_df['State'].fillna('', inplace=True)
 
 # Print the updated coordinates_df
 print(coordinates_df)
 
-
-# Print the updated coordinates_df
-
+# Save the updated coordinates_df to a JSON file
 coordinates_df.to_json("web/coordinates_dataset.json", orient="records")
 
-print(coordinates_df.head(10))
+print(coordinates_df)
 coordinates_df.info()
--- data/extraxt_crossing_coordinates.py
+++ data/extraxt_crossing_coordinates.py
@@ -1,26 +1,26 @@
 import pandas as pd
 
-df = pd.read_csv('data/border_crossing_data/FINAL_border_crossing.csv')
+df = pd.read_json('data/border_crossing_data/FINAL_border_crossing.json')
 
-coordinates_df = df.groupby('PortCode').agg({'Point': 'first'}).reset_index()
+coordinates_df = df.groupby('PortCode').agg({'Point': 'first', 'State':'first'}).reset_index()
 
-coordinates_df[['Longitude', 'Latitude']] = coordinates_df['Point'].str.split(',', expand=True)
+#coordinates_df[['Longitude', 'Latitude']] = coordinates_df['Point'].str.split(',', expand=True)
 
 coordinates_df['Longitude'] = coordinates_df['Longitude'].astype(float)
 coordinates_df['Latitude'] = coordinates_df['Latitude'].astype(float)
 
-coordinates_df.drop(columns=['Point'], inplace=True)
+#coordinates_df.drop(columns=['Point'], inplace=True)
 
 coordinates_df = coordinates_df[['PortCode', 'Latitude', 'Longitude',]]
 
 coordinates_df.info()
 print(coordinates_df.head(20))
 
-df = df.drop(columns=['Point'])
+#df = df.drop(columns=['Point'])
 
 df.info()
 print(df.head(20))
 
 
 df.to_json("data/border_crossing_data/FINAL_border_crossing.json", orient='records')
-coordinates_df.to_json("data/border_crossing_data/coordinates_dataset.json", orient='records')
+coordinates_df.to_json("web/coordinates_dataset.json", orient='records')
