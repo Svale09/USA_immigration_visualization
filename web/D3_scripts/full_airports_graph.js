813c1cce1b637375a4bcdecb9c44a3d4dae1a359
Svale09 <svalinaivan09+git@gmail.com>
2024-05-31 23:54:50 +0200

    Added tooltip to the flights ful graph

--- web/D3_scripts/full_airports_graph.js
+++ web/D3_scripts/full_airports_graph.js
@@ -1,9 +1,7 @@
 // Define dimensions for the chart
 var margin = { top: 20, right: 20, bottom: 10, left: 60 };
-      var width = 700 - margin.left - margin.right;
-      var height = 400 - margin.top - margin.bottom;
-
-// Create SVG element
+var width = 700 - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
 
 // Load the data
 d3.json("FINAL_flightsJSON.json", function (error, data) {
@@ -53,21 +51,21 @@ d3.json("FINAL_flightsJSON.json", function (error, data) {
       })
     );
 
-    var yAxis = d3.svg
+  var yAxis = d3.svg
     .axis()
     .scale(y)
     .orient("left")
     .tickFormat(function (d) {
-      return d / 1000000; // Divide by 1 million and append "M"
+      return d / 1000000;
     });
 
   var svg = d3
     .select("#airports_full")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom*2)
+    .attr("height", height + margin.top + margin.bottom * 4)
     .append("g")
-    .attr("transform", "translate(" + margin.left + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   // Add the axes
   svg
@@ -84,7 +82,6 @@ d3.json("FINAL_flightsJSON.json", function (error, data) {
   svg
     .append("g")
     .attr("class", "y axis")
-    .attr("transform", "translate(0," + 6 + ")")
     .call(yAxis)
     .append("text")
     .attr("transform", "rotate(-90)")
@@ -112,4 +109,40 @@ d3.json("FINAL_flightsJSON.json", function (error, data) {
     .attr("d", line)
     .style("stroke", "steelblue")
     .style("fill", "none");
+
+  // Create a tooltip element
+  var graphTooltip = d3
+    .select("body")
+    .append("div")
+    .attr("class", "graph-tooltip");
+
+  // Add a mousemove event listener to the graph container
+  svg.on("mousemove", function () {
+    var mouseX = d3.mouse(this)[0]; // Get the x position of the mouse relative to the graph
+
+    // Find the nearest x value in the graph data
+    var nearestDataPoint = nestedData.reduce(function (prev, curr) {
+      var prevX = x(prev.key);
+      var currX = x(curr.key);
+      return Math.abs(currX - mouseX) < Math.abs(prevX - mouseX) ? curr : prev;
+    });
+
+    // Update the tooltip with the x and y values
+    graphTooltip
+      .style("left", d3.event.pageX + 10 + "px") // Position the tooltip to the right of the mouse
+      .style("top", d3.event.pageY - 10 + "px") // Position the tooltip above the mouse
+      .style("opacity", 1)
+      .html(
+        "Year: " +
+          nearestDataPoint.key +
+          "<br/>Total Passengers: " +
+          +(nearestDataPoint.values / 1000000).toFixed(2) +
+          " mil"
+      );
+  });
+
+  // Add a mouseout event listener to hide the tooltip when the mouse leaves the graph
+  svg.on("mouseout", function () {
+    graphTooltip.style("opacity", 0);
+  });
 });
